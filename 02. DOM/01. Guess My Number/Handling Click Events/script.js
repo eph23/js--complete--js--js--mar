7878a4217781db05743f45d0fe6d14483f4a86f2
Eph23 <ephraims.818@gmail.com>
2026-04-01 04:58:38 +0600

    JS--COMPLETE-JS -- DOM -- 02. Handling Click Events

--- 02. DOM/01. Guess My Number/Handling Click Events/script.js
+++ 02. DOM/01. Guess My Number/Handling Click Events/script.js
@@ -5,3 +5,11 @@ const number = document.querySelector('.number');
 const score = document.querySelector('.score');
 const guessInput = document.querySelector('.guess');
 const btnCheck = document.querySelector('.check');
+
+btnCheck.addEventListener('click', function () {
+  const guess = Number(guessInput.value);
+
+  if (!guess) {
+    message.textContent = '🚫 No number!';
+  }
+});
