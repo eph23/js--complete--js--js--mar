db9c798c21b5b1eb8748de3e9fc524527a788d6b
Eph23 <ephraims.818@gmail.com>
2026-04-01 04:43:37 +0600

    JS--COMPLETE-JS -- DOM -- 01. Selecting and Manipulating Elements

---
+++ 02. DOM/01. Guess My Number/01. Selecting and Manipulating Elements/script.js
@@ -0,0 +1,7 @@
+'use strict';
+
+document.querySelector('.message').textContent = '🎉 Correct Number';
+document.querySelector('.number').textContent = 5;
+document.querySelector('.score').textContent = 50;
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
