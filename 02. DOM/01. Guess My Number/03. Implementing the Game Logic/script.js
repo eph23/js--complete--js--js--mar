8ce8b7c6a7d1eb53eb666dbd23cb111151710090
Eph23 <ephraims.818@gmail.com>
2026-04-01 05:15:07 +0600

    JS--COMPLETE-JS -- DOM -- 03. Implementing the Game Logic

---
+++ 02. DOM/01. Guess My Number/03. Implementing the Game Logic/script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const messageEl = document.querySelector('.message');
+const numberEl = document.querySelector('.number');
+const scoreEl = document.querySelector('.score');
+const guessInput = document.querySelector('.guess');
+const btnCheck = document.querySelector('.check');
+
+let score = 20;
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+
+numberEl.textContent = secretNumber;
+
+btnCheck.addEventListener('click', function () {
+  const guess = Number(guessInput.value);
+
+  if (!guess) {
+    messageEl.textContent = '🚫 No number!';
+  } else if (guess === secretNumber) {
+    messageEl.textContent = '✅YOU GUESSED IT RIGHT!!!';
+  } else if (guess > secretNumber) {
+    if (score > 0) {
+      score--;
+      messageEl.textContent = '📈 Too high';
+      scoreEl.textContent = score;
+    } else {
+      messageEl.textContent = '💥YOU LOST';
+      scoreEl.textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 0) {
+      score--;
+      messageEl.textContent = '📈 Too low';
+      scoreEl.textContent = score;
+    } else {
+      messageEl.textContent = '💥YOU LOST';
+      scoreEl.textContent = 0;
+    }
+  }
+});
