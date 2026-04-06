fa3940b677a84a0eec6dbd371f974b7e4b7ea09a
Eph23 <ephraims.818@gmail.com>
2026-04-06 20:47:33 +0600

    JS--COMPLETE-JS -- DATA-STRUCTURES -- 05. Short Circuiting

---
+++ 04. Data Structures/05. Short Circuiting/script.js
@@ -0,0 +1,72 @@
+'use strict';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.starterMenu[mainIndex]];
+  },
+
+  // Object as arguments
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 1,
+    address,
+    time = '22:00',
+  }) {
+    console.log(
+      `We have received your order! Your ${this.mainMenu[mainIndex]} and ${this.starterMenu[starterIndex]} will be delivered to ${address} at ${time}`,
+    );
+  },
+
+  // Spread operator as function argument
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(`Here is your pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+
+  orderPizza: function (mainIng, ...otherIng) {
+    console.log(mainIng, otherIng);
+  },
+};
+
+// Short circuiting
+// The OR (||) operator returns the first truthy value if available
+console.log(3 || 'Eph');
+console.log('' || 'Eph');
+console.log(true || 0);
+console.log(undefined || null);
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+// The AND (&&) operator returns the first falsy value if available
+console.log(0 && 'Eph');
+console.log(7 && 'Eph');
+console.log('Hello' && 23 && null && 'Ephraim');
+
+restaurant.orderPizza &&
+  restaurant.orderPizza('Mushrooms', 'Tomato', 'Spinach');
