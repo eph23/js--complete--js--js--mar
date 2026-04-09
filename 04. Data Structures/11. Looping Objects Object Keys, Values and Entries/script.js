f7a6059763bba9f6b3a46954c43e98b2dcec5855
Eph23 <ephraims.818@gmail.com>
2026-04-09 06:16:29 +0600

    JS--COMPLETE-JS -- DATA-STRUCTURES -- 11. Looping Objects Object Keys, Values and Entries

---
+++ 04. Data Structures/11. Looping Objects Object Keys, Values and Entries/script.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [days[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [days[4]]: {
+    open: 11,
+    close: 23,
+  },
+  sat: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours,
+
+  order(starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.starterMenu[mainIndex]];
+  },
+
+  // Object as arguments
+  orderDelivery({ starterIndex = 1, mainIndex = 1, address, time = '22:00' }) {
+    console.log(
+      `We have received your order! Your ${this.mainMenu[mainIndex]} and ${this.starterMenu[starterIndex]} will be delivered to ${address} at ${time}`,
+    );
+  },
+
+  // Spread operator as function argument
+  orderPasta(ing1, ing2, ing3) {
+    console.log(`Here is your pasta with ${ing1}, ${ing2} and ${ing3}`);
+  },
+
+  orderPizza(mainIng, ...otherIng) {
+    console.log(mainIng, otherIng);
+  },
+};
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+for (const day of properties) {
+  console.log(day);
+}
+
+const values = Object.values(openingHours);
+console.log(values);
+
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for (const [key, { open, close }] of entries) {
+  console.log(`on ${key} we open at ${open} and close at ${close}`);
+}
