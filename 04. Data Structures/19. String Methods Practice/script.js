926f786b0d1dc63e213ef1d7712a4f1ec4b2476c
Eph23 <ephraims.818@gmail.com>
2026-04-14 05:24:44 +0600

    JS--COMPLETE-JS -- DATA-STRUCTURES -- 19. String Methods Practice

---
+++ 04. Data Structures/19. String Methods Practice/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+
+  const output = `${type.includes('_Delayed') ? '⚠️' : ''}${type.replaceAll('_', ' ')} from ${from.slice(0, 3).toUpperCase()} to ${to.slice(0, 3).toUpperCase()} (${time.replace(':', 'h')})`;
+
+  console.log(output.padStart('44', ' '));
+}
