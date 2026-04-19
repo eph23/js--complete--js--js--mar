cd058d14aa4c1530807478922dffe68670ce0e24
Eph23 <ephraims.818@gmail.com>
2026-04-19 11:52:29 +0600

    JS--COMPLETE-JS -- FUNCTIONS -- 09. Closure Examples

---
+++ 05. Functions/09. Closure Examples/script.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let f;
+
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+h();
+f();
+
+const boardPassengers = function (num, wait) {
+  const perGroup = num / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${num} passengers`);
+    console.log(`There are 3 groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
